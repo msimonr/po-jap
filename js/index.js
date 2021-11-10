14fc1f0d1f66bee72a803d93bb989145cd6474f9
msimonr <mauricio.simon.r@gmail.com>
2021-11-10 19:39:55 -0300

    animation

--- js/index.js
+++ js/index.js
@@ -1,40 +1,73 @@
+//canvas settings
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-const width = canvas.width = 320;
-const height = canvas.height = 480;
-const frameWidth = 541;
-const frameHeight = 514;
-const xPos = 130;
-const yPos = 160;
-const scale = 0.3;
-const fps = 60;
-const secondsToUpdate = 1 * fps;
+const width = canvas.width = 320; //ancho canvas
+const height = canvas.height = 480; //alto canvas
 
-let count = 0;
+//sprite settings
+const frameWidth = 200; //ancho de cada sprite
+const frameHeight = 200; //alto de cada sprite
+const xPos = 60; //pos de inicio sprite X (0 izquierda)
+const yPos = 160; //pos de inicio sprite Y (0 arriba)
+const scale = 1; //escala de sprite
+const timeToUpdate = 10; //tiempo de refresco para animacion
 
+let count = 0; //tiempo desde ultima actualizacion de animacion
+
+//carga de hoja de sprites
 const sprites = new Image();
-sprites.src = "img/spritePrueba.png";
+sprites.src = "img/spritePrueba1.png";
 
-canvas.style.marginTop = window.innerHeight / 2 - height / 2 + "px";
+const State = {
+    states: {}, // estados creados
+    generateState: function(name, startIndex, endIndex) {
+        if (!this.states[name]) {
+            this.states[name] = {
+                frameIndex: startIndex,
+                startIndex: startIndex,
+                endIndex: endIndex
+            };
+        }
+    },
+    getState: function(name) {
+        if (this.states[name]) {
+            return this.states[name];
+        }
+    }
+};
 
+//:::: Generar estados ::::
 
-function animate() {
+State.generateState('walkRight', 0, 1);
+
+function animate(state) {
     context.drawImage(
         sprites,
+        state.frameIndex * frameWidth,
         0,
-        0,
-        1 * frameWidth,
+        frameWidth,
         frameHeight,
         xPos,
         yPos,
         frameWidth * scale,
         frameHeight * scale
     );
+    console.log(state);
+
+    count++;
+    if (count > timeToUpdate) {
+        console.log(state);
+        state.frameIndex++;
+        count = 0;
+    }
+    if (state.frameIndex > state.endIndex) {
+        state.frameIndex = state.startIndex;
+    }
 }
 
 function frame() {
     context.clearRect(0, 0, width, height);
-    animate();
+    animate(State.getState('walkRight'));
     requestAnimationFrame(frame);
 }
 
